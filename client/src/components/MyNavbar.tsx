0b468c31a0201bb41cc8e3172e094b92e094b2c1
David Ahmadov <ahmedavid@gmail.com>
2023-07-12 07:12:45 +0000

    ghjhgjg

--- client/src/components/MyNavbar.tsx
+++ client/src/components/MyNavbar.tsx
@@ -18,7 +18,7 @@ const MyNavbar = ({ onNewDoc, onNewCategory }: Props) => {
               New Category
             </Button>
 
-            <span>{window.API_URL} v3</span>
+            <span>{window.API_URL} v4 </span>
           </Nav>
         </Navbar.Collapse>
       </Container>
